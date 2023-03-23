c5ff6785bd85630e2604795c38ac8d8c57b5f64c
bohzhang1 <bohan.zhang4@student.unimelb.edu.au>
2023-03-24 00:39:45 +1100

    done

---
+++ bagel-starter-pack-1/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        loop1:
+        for (int i =0;i<2;i++){
+            if (i==1){
+                break loop1;
+            }
+
+
+    }
+
+
+    }
+}
